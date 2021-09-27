03ce1dcd7846c8283534bbc6c1c8945b6ecd4218
孙许 <2572012446@qq.com>
2021-09-28 00:23:18 +0800

    学习feign

--- src/main/java/com/sunxu/ServiceAClient.java
+++ src/main/java/com/sunxu/ServiceAClient.java
@@ -1,9 +1,7 @@
 package com.sunxu;
 
 import com.sunxu.demo.ServiceAInterface;
-import com.sunxu.demo.User;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.*;
 
 /**
  * @author 孙许
@@ -15,6 +13,9 @@ import org.springframework.web.bind.annotation.*;
  * feign发起请求的时候,都会交给ribbon做负载均衡
  *
  * 我只是说,我要访问ServiceA,但是人家里面ServiceA里面定义哪些接口哪些参数,就不需要管了
+ * 这里不用把service的接口重新写一遍,直接复用别人jar中提供的就ok了
+ *
+ * 这里类似于dubbo @Reference注解类似
  */
 @FeignClient("ServiceA")
 public interface ServiceAClient extends ServiceAInterface {
